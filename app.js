4bf18c67dc124538120b6d75780c6ae002fed946
= <pathan.sameer3@gmail.com>
2019-10-04 15:59:24 +0530

    added express server to send message to the producer

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const pushEvent = require('./producer.js').pushEvent;
+const config = require('./config')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3005, function(){
+	console.log('app running on port 3005');
+});
+
+app.post('/push-event', function(req,res){
+	console.log('hit / on server');
+	const message = req.body.message || "Default message";
+	pushEvent( [{
+      topic: config.kafka_topic,
+      messages: message    
+  }]);
+	res.send('success');
+
+});
--- producer.js
+++ producer.js
@@ -1,12 +1,22 @@
 const kafka = require('kafka-node');
 const bp = require('body-parser');
 const config = require('./config');
+const kafka_topic = config.kafka_topic;
+
+pushEvent = (payloads) => {
+ let push_status = producer.send(payloads, (err, data) => {
+      if (err) {
+        console.log('[kafka-producer -> '+kafka_topic+']: broker update failed');
+      } else {
+        console.log('[kafka-producer -> '+kafka_topic+']: broker update success');
+      }
+    });
+}
 
 try {
   const Producer = kafka.Producer;
   const client = new kafka.KafkaClient("localhost:9092");
   var producer = new Producer(client);
-  const kafka_topic = config.kafka_topic;
   console.log(kafka_topic);
   let payloads = [
     {
@@ -16,13 +26,7 @@ try {
   ];
 
   producer.on('ready', async function() {
-    let push_status = producer.send(payloads, (err, data) => {
-      if (err) {
-        console.log('[kafka-producer -> '+kafka_topic+']: broker update failed');
-      } else {
-        console.log('[kafka-producer -> '+kafka_topic+']: broker update success');
-      }
-    });
+    console.log('producer ready');
   });
 
   producer.on('error', function(err) {
@@ -33,4 +37,8 @@ try {
 }
 catch(e) {
   console.log(e);
+}
+
+module.exports = {
+  pushEvent
 }
